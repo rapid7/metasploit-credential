5b84ce01a1ae4ac3b685d9a9029f9c3786d0695b
Luke Imhoff <luke_imhoff@rapid7.com>
2014-03-28 15:44:48 -0500

    Add semantic versioning 2.0 support
    MSP-9604

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -1,5 +1,22 @@
 module Metasploit
   module Credential
-    VERSION = "0.0.1"
+    module Version
+      MAJOR = 0
+      MINOR = 0
+      PATCH = 1
+      PRERELEASE = 'gem-skeleton'
+
+      def self.full
+        version = "#{MAJOR}.#{MINOR}.#{PATCH}"
+
+        if defined? PRERELEASE
+          version = "#{version}-#{PRERELEASE}"
+        end
+
+        version
+      end
+    end
+
+    VERSION = Version.full
   end
 end
