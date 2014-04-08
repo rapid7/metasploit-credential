fd252ccec83434869684565c04e1b5e1b27252d0
Luke Imhoff <luke_imhoff@rapid7.com>
2014-04-08 13:24:03 -0500

    Use ignored password_data as data to hash for PasswordHash factory
    MSP-9605

--- spec/factories/metasploit/credential/password_hashes.rb
+++ spec/factories/metasploit/credential/password_hashes.rb
@@ -4,10 +4,12 @@ FactoryGirl.define do
           # uses its own data sequence to differentiate password hashes from other private data and #type is
           # automatically set by ActiveRecord because Metasploit::Credential::Password is an STI subclass.
           class: Metasploit::Credential::Password do
-    data { generate :metasploit_credential_password_data }
-  end
+    ignore do
+      password_data { generate :metasploit_credential_password_data }
+    end
 
-  sequence :metasploit_credential_password_hash_data do |n|
-    Bcrypt::Password.create("metasploit_credential_password_hash_data#{n}").hash
+    data {
+      BCrypt::Password.create(password_data).hash
+    }
   end
 end
