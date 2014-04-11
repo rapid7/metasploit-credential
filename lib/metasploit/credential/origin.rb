dc47df5bb618e1a922a1bfc42c1e78ccf79e033f
Luke Imhoff <luke_imhoff@rapid7.com>
2014-04-11 09:16:41 -0500

    Fix return type of Metasploit::Credential::Origin.table_name_prefix in docs
    MSP-9605

--- lib/metasploit/credential/origin.rb
+++ lib/metasploit/credential/origin.rb
@@ -1,6 +1,8 @@
 # Namespace for `Metasploit::Credential::Core#origin`s for `Metasploit::Credential::Core`s.
 module Metasploit::Credential::Origin
   # The prefix for table name of `ActiveRecord::Base` subclasses in the namespace.
+  #
+  # @return [String] `'metasploit_credential_origin_'`
   def self.table_name_prefix
     'metasploit_credential_origin_'
   end
