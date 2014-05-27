43ca69b0fad5877bab8b5e94b7b73d3a4b678a48
Trevor Rosen <trevor_rosen@rapid7.com>
2014-05-27 15:05:02 -0500

    Fix some spec failures in Factories
    MSP-9945

--- spec/factories/metasploit/credential/importer/csv/manifests.rb
+++ spec/factories/metasploit/credential/importer/csv/manifests.rb
@@ -2,13 +2,14 @@ FactoryGirl.define do
   factory :metasploit_credential_zip_importer_manifest_well_formed_compliant,
           class: Metasploit::Credential::Importer::CSV::Manifest  do
 
-    origin_import { FactoryGirl.build :metasploit_credential_origin_import }
+    origin { FactoryGirl.build :metasploit_credential_origin_import }
     data { generate :well_formed_csv_manifest_io }
   end
 
   sequence :well_formed_csv_manifest_io do |n|
     csv_string =<<-eos
-
+username,ssh_key_file_name
+fraggle,fraggle.pem
     eos
   end
 end
