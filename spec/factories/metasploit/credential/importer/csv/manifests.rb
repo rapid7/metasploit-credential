a38a0757d89d260398cb4ad6dfc02505f5eba0c6
Trevor Rosen <trevor_rosen@rapid7.com>
2014-05-28 11:59:25 -0500

    Make some more specs pass; code review changes

--- spec/factories/metasploit/credential/importer/csv/manifests.rb
+++ spec/factories/metasploit/credential/importer/csv/manifests.rb
@@ -6,9 +6,23 @@ FactoryGirl.define do
     data { generate :well_formed_csv_manifest_io }
   end
 
+  factory :metasploit_credential_zip_importer_manifest_malformed,
+          class: Metasploit::Credential::Importer::CSV::Manifest  do
+
+    origin { FactoryGirl.build :metasploit_credential_origin_import }
+    data { generate :malformed_csv_manifest_io }
+  end
+
   sequence :well_formed_csv_manifest_io do |n|
     csv_string =<<-eos
 username,ssh_key_file_name
+fraggle,fraggle.pem
+    eos
+  end
+
+  sequence :malformed_csv_manifest_io do |n|
+    csv_string =<<-eos
+bad_header1,bad_header2
 fraggle,fraggle.pem
     eos
   end
--- spec/lib/metasploit/credential/importer/csv/manifest_spec.rb
+++ spec/lib/metasploit/credential/importer/csv/manifest_spec.rb
@@ -1,13 +1,19 @@
 require 'spec_helper'
 
 describe Metasploit::Credential::Importer::CSV::Manifest do
+  include_context 'Mdm::Workspace'
+
   describe "#validation" do
     describe "with proper headers" do
-      # it { should be_valid }
+      subject(:well_formed_manifest){ FactoryGirl.build(:metasploit_credential_zip_importer_manifest_well_formed_compliant)}
+
+      it { should be_valid }
     end
 
     describe "without proper headers" do
-      # it { should_not be_valid }
+      subject(:malformed_manifest){ FactoryGirl.build(:metasploit_credential_zip_importer_manifest_malformed)}
+
+      it { should_not be_valid }
     end
   end
 
