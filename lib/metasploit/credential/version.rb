39eb7a2670de28055c044a2c74d2693cd0b677ea
Trevor Rosen <trevor_rosen@rapid7.com>
2014-06-18 11:25:50 -0500

    Version bump

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -7,9 +7,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 4
       # The patch number, scoped to the {MINOR} version number.
-      PATCH = 2
+      PATCH = 4
       # The prerelease name of the given {MAJOR}.{MINOR}.{PATCH} version number. Will not be defined on master.
-      PRERELEASE = 'originating-host-id-scope-sucks'
+      PRERELEASE = 'electro-release'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
