a36b024935b655d5764483b509baa180f843fab3
Luke Imhoff <luke_imhoff@rapid7.com>
2014-06-20 13:08:48 -0500

    Metasploit::Credential::Export docs
    MSP-10130

--- lib/metasploit/credential/exporter.rb
+++ lib/metasploit/credential/exporter.rb
@@ -1,3 +1,5 @@
+# Namespace for classes which export {Metasploit::Credential::Core Metasploit::Credential::Cores} or
+# {Metasploit::Credential::Login Metasploit::Credential::Logins}.
 module Metasploit::Credential::Exporter
   extend ActiveSupport::Autoload
 
