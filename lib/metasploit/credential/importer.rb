fa786ee81d1aaa0d05196b3a2ac912c9ce889ba2
Luke Imhoff <luke_imhoff@rapid7.com>
2014-06-20 14:05:59 -0500

    Document Metasploit::Credential::Importer
    MSP-10130

--- lib/metasploit/credential/importer.rb
+++ lib/metasploit/credential/importer.rb
@@ -1,3 +1,5 @@
+# Namespace for all importers of {Metasploit::Credential::Core Metasploit::Credential::Cores} and
+# {Metasploit::Credential::Login Metasploit::Credential::Logins}.
 module Metasploit::Credential::Importer
   extend ActiveSupport::Autoload
 
