c3ce79518a22eaac05a8c23eeb7c6d0eabddeb63
David Maloney <DMaloney@rapid7.com>
2014-07-03 14:43:25 -0500

    pre-release tag

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -9,7 +9,7 @@ module Metasploit
       # The patch number, scoped to the {MINOR} version number.
       PATCH = 2
       # The prerelease name of the given {MAJOR}.{MINOR}.{PATCH} version number. Will not be defined on master.
-      PRERELEASE = 'task-associations'
+      PRERELEASE = 'electro-release'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
