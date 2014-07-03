a999a4493d1542ddac468baa8dfcfee6c0ead6a8
Trevor Rosen <trevor_rosen@rapid7.com>
2014-07-03 12:05:08 -0500

    Remove PRERELEASE from version.rb

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -8,8 +8,6 @@ module Metasploit
       MINOR = 5
       # The patch number, scoped to the {MINOR} version number.
       PATCH = 1
-      # The prerelease name of the given {MAJOR}.{MINOR}.{PATCH} version number. Will not be defined on master.
-      PRERELEASE = 'readme'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
