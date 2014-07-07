76a047a2768b6155cebae9de0076931363cdfa27
Lance Sanchez <lance.sanchez@rapid7.com>
2014-07-07 16:05:19 -0500

    Updating the PRERELEASE in version.rb
    changing the gemspec to reflect mdm's release

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -9,7 +9,7 @@ module Metasploit
       # The patch number, scoped to the {MINOR} version number.
       PATCH = 5
       # The prerelease version, scoped to the {MINOR} version number.
-      PRERELEASE = 'compatibility'
+      PRERELEASE = 'electro-release'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
