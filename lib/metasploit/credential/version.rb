d3467d4e59b1a9518cfb19677f0a5768f05c8e00
David Maloney <DMaloney@rapid7.com>
2014-07-09 10:32:59 -0500

    version bump minor
    should have bumped minor not patch

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -5,9 +5,9 @@ module Metasploit
       # The major version number.
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
-      MINOR = 5
+      MINOR = 6
       # The patch number, scoped to the {MINOR} version number.
-      PATCH = 7
+      PATCH = 0
 
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
