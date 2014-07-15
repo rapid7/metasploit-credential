8defe69dc28be3aeb0a16734b81dba3ffc5a211e
Samuel Huckins <samuel_huckins@rapid7.com>
2014-07-14 23:45:28 -0500

    Setting pre-release
    MSP-9842

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -9,7 +9,7 @@ module Metasploit
       # The patch number, scoped to the {MINOR} version number.
       PATCH = 1
       # The prerelease name of the given {MAJOR}.{MINOR}.{PATCH} version number. Will not be defined on master.
-      PRERELEASE = "electro-release"
+      PRERELEASE = "core-host-relation-scope-update"
 
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
