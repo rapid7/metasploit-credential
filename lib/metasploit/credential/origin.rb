8e90fd05e9919a66fc5a72f47b1f0fb46493a80a
Matt Buck <techpeace@gmail.com>
2014-07-24 13:43:22 -0500

    Add constants needed for filtering in the Pro UI
    MSP-10858

--- lib/metasploit/credential/origin.rb
+++ lib/metasploit/credential/origin.rb
@@ -1,5 +1,18 @@
 # Namespace for `Metasploit::Credential::Core#origin`s for `Metasploit::Credential::Core`s.
 module Metasploit::Credential::Origin
+  #
+  # CONSTANTS
+  #
+
+  # All valid origin types
+  VALID_TYPES = [
+      Metasploit::Credential::Origin::CrackedPassword,
+      Metasploit::Credential::Origin::Import,
+      Metasploit::Credential::Origin::Manual,
+      Metasploit::Credential::Origin::Service,
+      Metasploit::Credential::Origin::Session
+  ].collect { |klass| klass.model_name.human }
+
   # The prefix for table name of `ActiveRecord::Base` subclasses in the namespace.
   #
   # @return [String] `'metasploit_credential_origin_'`
