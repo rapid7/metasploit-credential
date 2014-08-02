4e8fe5d9c91ce301aaa337653442450efcfe6707
Samuel Huckins <samuel_huckins@rapid7.com>
2014-08-02 17:18:19 -0500

    Updates post PR 69 merge
    MSP-10963

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -8,8 +8,6 @@ module Metasploit
       MINOR = 8
       # The patch number, scoped to the {MINOR} version number.
       PATCH = 6
-      # The pre-release version, scoped to the {MINOR} version number.
-      PRERELEASE = 'realm-uniqueness'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
