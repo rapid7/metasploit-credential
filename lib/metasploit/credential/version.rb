829995876b2e8d120b58f24bec45e6710d8935f2
Joe Vennix <joe_vennix@rapid7.com>
2014-09-03 11:08:13 -0500

    Add PRERELEASE thingy.

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -8,6 +8,8 @@ module Metasploit
       MINOR = 9
       # The patch number, scoped to the {MINOR} version number.
       PATCH = 1
+      # delete me.
+      PRERELEASE = 'cred-search-operator-help'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
