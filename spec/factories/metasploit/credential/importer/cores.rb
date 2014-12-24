4989de7c05fcde1e8e3fb8e47a70ce2bdd25e5da
Samuel Huckins <samuel_huckins@rapid7.com>
2014-12-24 15:13:08 -0600

    DRYing up CSV header definitions
    MSP-11905

--- spec/factories/metasploit/credential/importer/cores.rb
+++ spec/factories/metasploit/credential/importer/cores.rb
@@ -1,20 +1,23 @@
 FactoryGirl.define do
+  long_form_headers  = 'username,private_type,private_data,realm_key,realm_value,host_address,service_port,service_name,service_protocol'
+  short_form_headers = 'username,private_data'
+
   factory :metasploit_credential_core_importer,
           class: Metasploit::Credential::Importer::Core do
-
-    origin {FactoryGirl.build :metasploit_credential_origin_import }
-    input { generate(:well_formed_csv_compliant_header)}
+            origin { FactoryGirl.build :metasploit_credential_origin_import }
+            input  { generate(:well_formed_csv_compliant_header) }
   end
+
 
   # Well-formed CSV
   # Has a compliant header as defined by Metasploit::Credential::Importer::Core
   # Contains 2 realms
   sequence :well_formed_csv_compliant_header do |n|
     csv_string =<<-eos
-username,private_type,private_data,realm_key,realm_value,host_address,service_port,service_name,service_protocol
 han_solo-#{n},#{Metasploit::Credential::Password.name},falcon_chief,#{Metasploit::Model::Realm::Key::ACTIVE_DIRECTORY_DOMAIN},Rebels
 princessl-#{n},#{Metasploit::Credential::Password.name},bagel_head,#{Metasploit::Model::Realm::Key::ACTIVE_DIRECTORY_DOMAIN},Rebels
 lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,#{Metasploit::Model::Realm::Key::ORACLE_SYSTEM_IDENTIFIER},dstar_admins
+#{long_form_headers}
     eos
     StringIO.new(csv_string)
   end
@@ -24,10 +27,10 @@ lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,#{Metasploit:
   # Contains 2 logins
   sequence :well_formed_csv_compliant_header_with_service_info do |n|
     csv_string =<<-eos
-username,private_type,private_data,realm_key,realm_value,host_address,service_port,service_name,service_protocol
 han_solo-#{n},#{Metasploit::Credential::Password.name},falcon_chief,#{Metasploit::Model::Realm::Key::ACTIVE_DIRECTORY_DOMAIN},Rebels,10.0.1.1,1234,smb,tcp
 princessl-#{n},#{Metasploit::Credential::Password.name},bagel_head,#{Metasploit::Model::Realm::Key::ACTIVE_DIRECTORY_DOMAIN},Rebels,10.0.1.2,1234,smb,tcp
 lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,#{Metasploit::Model::Realm::Key::ORACLE_SYSTEM_IDENTIFIER},dstar_admins
+#{long_form_headers}
     eos
     StringIO.new(csv_string)
   end
@@ -37,10 +40,10 @@ lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,#{Metasploit:
   # Contains no realm data
   sequence :well_formed_csv_compliant_header_no_realm do |n|
     csv_string =<<-eos
-username,private_type,private_data,realm_key,realm_value,host_address,service_port,service_name,service_protocol
 han_solo-#{n},#{Metasploit::Credential::Password.name},falcon_chief,,
 princessl-#{n},#{Metasploit::Credential::Password.name},bagel_head,,
 lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,,
+#{long_form_headers}
     eos
     StringIO.new(csv_string)
   end
@@ -51,10 +54,10 @@ lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,,
   # Contains core with a blank Public
   sequence :well_formed_csv_compliant_header_missing_public do |n|
     csv_string =<<-eos
-username,private_type,private_data,realm_key,realm_value,host_address,service_port,service_name,service_protocol
 ,#{Metasploit::Credential::Password.name},falcon_chief,#{Metasploit::Model::Realm::Key::ACTIVE_DIRECTORY_DOMAIN},Rebels
 princessl-#{n},#{Metasploit::Credential::Password.name},bagel_head,#{Metasploit::Model::Realm::Key::ACTIVE_DIRECTORY_DOMAIN},Rebels
 lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,#{Metasploit::Model::Realm::Key::ORACLE_SYSTEM_IDENTIFIER},dstar_admins
+#{long_form_headers}
     eos
     StringIO.new(csv_string)
   end
@@ -65,7 +68,7 @@ lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,#{Metasploit:
   # Contains core with a blank Private
   sequence :well_formed_csv_compliant_header_missing_private do |n|
     csv_string =<<-eos
-username,private_type,private_data,realm_key,realm_value,host_address,service_port,service_name,service_protocol
+#{long_form_headers}
 han_solo,,,#{Metasploit::Model::Realm::Key::ACTIVE_DIRECTORY_DOMAIN},Rebels
 princessl-#{n},#{Metasploit::Credential::Password.name},bagel_head,#{Metasploit::Model::Realm::Key::ACTIVE_DIRECTORY_DOMAIN},Rebels
 lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,#{Metasploit::Model::Realm::Key::ORACLE_SYSTEM_IDENTIFIER},dstar_admins
@@ -78,7 +81,7 @@ lord_vader-#{n},#{Metasploit::Credential::Password.name},evilisfun,#{Metasploit:
   # Conforms to "short" form, in which only username and private_data are specified in the file
   sequence :short_well_formed_csv do |n|
     csv_string =<<-eos
-username,private_data
+#{short_form_headers}
 han_solo-#{n},falC0nBaws
 princessl-#{n},bagelHead
     eos
@@ -116,7 +119,7 @@ foo,{"""}
   # We have a header row but nothing else
   sequence :empty_core_csv do |n|
     csv_string =<<-eos
-username,private_type,private_data,realm_key,realm_value,host_address,service_port,service_name,service_protocol
+#{long_form_headers}
     eos
     StringIO.new(csv_string)
   end
