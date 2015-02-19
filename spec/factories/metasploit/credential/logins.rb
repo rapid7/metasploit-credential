af04f109b2f07b4a0cf8f3cb866b93ab43ab7338
Sonny Gonzalez <sonny_gonzalez@rapid7.com>
2015-02-19 14:24:23 -0600

    metasploit-credential - remove Rails 4 warnings
    MSP-12174
    
    * change PRERELEASE constant to reflect branch to make specs pass
    * remove config.whiny_nils
    * Change factories to use #transient instead of #ignore
    
    Conflicts:
    	lib/metasploit/credential/version.rb

--- spec/factories/metasploit/credential/logins.rb
+++ spec/factories/metasploit/credential/logins.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :metasploit_credential_login,
           class: Metasploit::Credential::Login do
-    ignore do
+    transient do
       host {
         FactoryGirl.build(
           :mdm_host, workspace: workspace
--- spec/factories/metasploit/credential/ssh_keys.rb
+++ spec/factories/metasploit/credential/ssh_keys.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :metasploit_credential_ssh_key,
           class: Metasploit::Credential::SSHKey do
-    ignore do
+    transient do
       key_type { generate :metasploit_credential_ssh_key_key_type }
       # key size tuned for speed.  DO NOT use for production, it is below current recommended key size of 2048
       key_size { 512 }
@@ -13,13 +13,13 @@ FactoryGirl.define do
     }
 
     factory :metasploit_credential_dsa_key do
-      ignore do
+      transient do
         key_type :DSA
       end
     end
 
     factory :metasploit_credential_rsa_key do
-      ignore do
+      transient do
         key_type :RSA
       end
     end
