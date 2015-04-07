11eb22c6fec3c822444698c319a30a5a97f0acea
Luke Imhoff <luke_imhoff@rapid7.com>
2015-04-07 12:09:15 -0500

    Update version for branch
    MSP-12519

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -6,8 +6,10 @@ module Metasploit
       MAJOR = 0
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 14
-      # The patch number, scoped to the {MINOR} version number.
-      PATCH = 3
+      # The patch number, scoped to the {MAJOR} and {MINOR} version number.
+      PATCH = 4
+      # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version number.
+      PRERELEASE = 'autoload'
 
       # The full version string, including the {MAJOR}, {MINOR}, {PATCH}, and optionally, the {PRERELEASE} in the
       # {http://semver.org/spec/v2.0.0.html semantic versioning v2.0.0} format.
