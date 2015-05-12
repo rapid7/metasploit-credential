2a5729f0bc0e00e4cf933c3a76b38e1e08a390b1
Luke Imhoff <luke_imhoff@rapid7.com>
2015-05-12 12:17:12 -0500

    Update version branch branch
    MSP-12707

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -13,9 +13,9 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 0
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 1
+      PATCH = 2
       # The prerelease version, scoped to the {MAJOR}, {MINOR}, and {PATCH} version numbers.
-      PRERELEASE = 'undocumented'
+      PRERELEASE = 'unresolved-links'
 
       #
       # Module Methods
