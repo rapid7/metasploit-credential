c67eaaa5120893dbfe940377383a50e358e88505
Greg Mikeska <greg_mikeska@rapid7.com>
2015-08-05 10:14:29 -0500

    add prerelease to version MSP-13025

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -13,7 +13,8 @@ module Metasploit
       # The minor version number, scoped to the {MAJOR} version number.
       MINOR = 0
       # The patch version number, scoped to the {MAJOR} and {MINOR} version numbers.
-      PATCH = 0
+      PATCH = 1
+      PRERELEASE = 'creds_csv_import'
 
       #
       # Module Methods
