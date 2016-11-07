f6d22488b5597a8a45ccda7a30452962279b696b
Lance Sanchez <lance.sanchez@rapid7.com>
2016-11-07 15:45:49 -0600

    removing some newer psql info
    from structure.sql
    
    MS-2242

--- spec/dummy/db/structure.sql
+++ spec/dummy/db/structure.sql
@@ -2,16 +2,12 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.5.3
--- Dumped by pg_dump version 9.5.3
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
 
 --
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
