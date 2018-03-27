7c45be10416af74a40876f3ffe6978da91eb208d
Brent Cook <busterb@gmail.com>
2018-03-27 10:49:30 -0500

    bump to 3.0.0

--- lib/metasploit/credential/version.rb
+++ lib/metasploit/credential/version.rb
@@ -3,13 +3,13 @@
 module Metasploit
   module Credential
     # VERSION is managed by GemRelease
-    VERSION = '2.0.15'
-  
+    VERSION = '3.0.0'
+
     # @return [String]
     #
     # returns the VERSION
     #
-  
+
     def self.version
       VERSION
     end
