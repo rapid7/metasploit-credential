3810049bef236cd5494f0c40830d4ed209c88002
Christophe De La Fuente <christophe_delafuente@rapid7.com>
2024-12-16 11:03:09 +0100

    Add CA and ADCS Template metadata to Pkcs12

--- spec/factories/metasploit/credential/pkcs12.rb
+++ spec/factories/metasploit/credential/pkcs12.rb
@@ -10,28 +10,57 @@ FactoryBot.define do
       subject { '/C=BE/O=Test/OU=Test/CN=Test' }
       # the cert issuer
       issuer { '/C=BE/O=Test/OU=Test/CN=Test' }
+      # the base64-encoded cert
+      pkcs12_base64 {
+        password = ''
+        pkcs12_name = ''
+
+        private_key = OpenSSL::PKey::RSA.new(key_size)
+        public_key = private_key.public_key
+
+        cert = OpenSSL::X509::Certificate.new
+        cert.subject = OpenSSL::X509::Name.parse(subject)
+        cert.issuer = OpenSSL::X509::Name.parse(issuer)
+        cert.not_before = Time.now
+        cert.not_after = Time.now + 365 * 24 * 60 * 60
+        cert.public_key = public_key
+        cert.serial = 0x0
+        cert.version = 2
+        cert.sign(private_key, OpenSSL::Digest.new(signing_algorithm))
+
+        pkcs12 = OpenSSL::PKCS12.create(password, pkcs12_name, private_key, cert)
+        Base64.strict_encode64(pkcs12.to_der)
+      }
     end
 
-    data {
-      password = ''
-      pkcs12_name = ''
-
-      private_key = OpenSSL::PKey::RSA.new(key_size)
-      public_key = private_key.public_key
-
-      cert = OpenSSL::X509::Certificate.new
-      cert.subject = OpenSSL::X509::Name.parse(subject)
-      cert.issuer = OpenSSL::X509::Name.parse(issuer)
-      cert.not_before = Time.now
-      cert.not_after = Time.now + 365 * 24 * 60 * 60
-      cert.public_key = public_key
-      cert.serial = 0x0
-      cert.version = 2
-      cert.sign(private_key, OpenSSL::Digest.new(signing_algorithm))
-
-      pkcs12 = OpenSSL::PKCS12.create(password, pkcs12_name, private_key, cert)
-      pkcs12_base64 = Base64.strict_encode64(pkcs12.to_der)
-      pkcs12_base64
-    }
+    data { Metasploit::Credential::Pkcs12.build_data(pkcs12: pkcs12_base64) }
   end
+
+  factory :metasploit_credential_pkcs12_with_ca, parent: :metasploit_credential_pkcs12 do
+    transient do
+      # The CA that issued the certificate
+      ca { "test-ca" }
+    end
+
+    data { Metasploit::Credential::Pkcs12.build_data(pkcs12: pkcs12_base64, ca: ca) }
+  end
+
+  factory :metasploit_credential_pkcs12_with_adcs_template, parent: :metasploit_credential_pkcs12 do
+    transient do
+      # The certificate template used to issue the certificate
+      adcs_template { "User" }
+    end
+
+    data { Metasploit::Credential::Pkcs12.build_data(pkcs12: pkcs12_base64, adcs_template: adcs_template) }
+  end
+
+  factory :metasploit_credential_pkcs12_with_ca_and_adcs_template, parent: :metasploit_credential_pkcs12 do
+    transient do
+      ca { "test-ca" }
+      adcs_template { "User" }
+    end
+
+    data { Metasploit::Credential::Pkcs12.build_data(pkcs12: pkcs12_base64, ca: ca, adcs_template: adcs_template) }
+  end
+
 end
